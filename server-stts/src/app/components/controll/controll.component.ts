b8dda7f92fd0eaa167e33fd403b70d2dc55186cf
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2025-01-15 16:40:33 -0300

    .

--- server-stts/src/app/components/controll/controll.component.ts
+++ server-stts/src/app/components/controll/controll.component.ts
@@ -1,11 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, HostBinding, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-controll',
   //imports: [],
   templateUrl: './controll.component.html',
-  styleUrl: './controll.component.css'
+  styleUrl: './controll.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControllComponent {
+
+  private el = inject(ElementRef);
+
   label = input.required<string>();
+
+  onClick(){
+    console.log("Clicked")
+    console.log(this.el)
+  }
 }
--- server-stts/src/app/dashboard/server-status/server-status.component.ts
+++ server-stts/src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
-  imports: [],
+  //imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random(); // 0 - 0.999
+
+      if (random > 0.5) {
+        this.currentStatus = 'online';
+        return;
+      }
+
+      this.currentStatus = 'offline';
+    }, 5000);
+  }
+
+
+
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
 }
